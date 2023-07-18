aa4f08ca7cf051027fd7876097b94df12e02ffe1
Ojidaemo <vit.martsinovich@gmail.com>
2023-07-18 10:36:14 +0300

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Vitali Martsinovich on 2023-07-18.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
